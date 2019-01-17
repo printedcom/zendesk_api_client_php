36ae574a4fe66c445fb97a699a92de1b1bf5fa2a
Alex Moon <alex.moon@printed.com>
2019-01-17 13:22:08 +0000

    Added object name to phone number

--- tests/Zendesk/API/UnitTests/Voice/PhoneNumbersTest.php
+++ tests/Zendesk/API/UnitTests/Voice/PhoneNumbersTest.php
@@ -2,6 +2,7 @@
 
 namespace Zendesk\API\UnitTests\Voice;
 
+use Faker\Factory;
 use Zendesk\API\UnitTests\BasicTest;
 
 class PhoneNumbersTest extends BasicTest
@@ -21,5 +22,25 @@ class PhoneNumbersTest extends BasicTest
         $this->assertEndpointCalled(function () use ($queryParams) {
             $this->client->voice->phoneNumbers()->search($queryParams);
         }, 'channels/voice/phone_numbers/search.json', 'GET', ['queryParams' => $queryParams]);
+    }
+
+    /**
+     * Tests if the client can call and build the update phone numbers endpoint
+     */
+    public function testUpdate()
+    {
+        $faker = Factory::create();
+        $id = $faker->numberBetween(1);
+        $params = [
+            'greeting_ids' => [
+                $faker->numberBetween(1000000000, 2000000000)
+            ],
+        ];
+
+        $this->assertEndpointCalled(function () use ($params, $id) {
+            $this->client->voice->phoneNumbers()->update($id, $params);
+        }, "channels/voice/phone_numbers/{$id}.json", 'PUT', [
+            'postFields' => ['phone_number' => $params],
+        ]);
     }
 }
